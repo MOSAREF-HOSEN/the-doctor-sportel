142911351fac0d0f11b358d3d6c6be3891570fef
Golap-mostofa <mostofa6410@gmail.com>
2022-05-12 17:57:29 +0600

    create sign up functionnality

--- src/pages/Sheard/Navbar.js
+++ src/pages/Sheard/Navbar.js
@@ -1,15 +1,23 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Navbar = () => {
+    const [user, loading, error] = useAuthState(auth);
+    const logout = () => {
+        signOut(auth);
+      };
     const manuItems = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/appointment'>Appointment</Link></li>
         <li><Link to='/review'>Review</Link></li>
         <li><Link to='/contact'>contact</Link></li>
         <li><Link to='/about'>About</Link></li>
-        <li><Link to='/login'>Login</Link></li>
+        <li>{user ? <button  onClick={logout} class="btn btn-ghost">sign Out</button> : <Link to='/signup'>Login</Link>}</li>
     </>
+    
     return (
 
         <div>
