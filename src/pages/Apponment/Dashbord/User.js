bbb21f5e97b1fedf5b82ddee82e6a386cb7e6480
Golap-mostofa <mostofa6410@gmail.com>
2022-05-15 21:56:37 +0600

    create userRow

--- src/pages/Apponment/Dashbord/User.js
+++ src/pages/Apponment/Dashbord/User.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../Sheard/Loading';
+import UserRow from './UserRow';
 
 const User = () => {
+    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()))
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+    console.log(users);
     return (
         <div>
-            <h2 className='text-2xl text-secondary'>All users</h2>
+            <h2 className='text-2xl text-secondary'>All users:{users.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       {
+                           users.map(user=><UserRow
+                           key={user._id}
+                           user={user}
+                           ></UserRow>)
+                       }
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
