7ecfb8696301642931198080bf6e766ca8d02e54
Golap-mostofa <mostofa6410@gmail.com>
2022-05-18 18:22:20 +0600

    credided card implement

---
+++ src/pages/Apponment/Dashbord/CheckoutForm.js
@@ -0,0 +1,56 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useState } from 'react';
+
+const CheckoutForm = () => {
+    const stripe = useStripe()
+    const elements = useElements()
+    const [cardError, setCardError] = useState('')
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (!stripe || !elements) {
+            return
+        }
+        const card = elements.getElement(CardElement)
+        if (card === null) {
+            return
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        setCardError(error?.message || '')
+
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                <button className='btn- btn-primary btn-sm mt-4' type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+            {
+                cardError && <p className='text-red-600'>{cardError}</p>
+            }
+        </div>
+    );
+};
+
+export default CheckoutForm;
--- src/pages/Apponment/Dashbord/Payment.js
+++ src/pages/Apponment/Dashbord/Payment.js
@@ -1,9 +1,13 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../../Sheard/Loading';
+import CheckoutForm from './CheckoutForm';
 
 const Payment = () => {
+    const stripePromise = loadStripe('pk_test_51L0f50IawdrGN2QNNfpnsVtZxtypMcOduvIOjMZGVHtw3JrS8VvjmlScnPOs6hqQDWeXdcVYNUxG4cboXDCom4tS00Ame6Z3CS');
     const { id } = useParams()
     const url = `http://localhost:5000/booking/${id}`
     const { data: appionment, isLoading } = useQuery(['booking', id], () => fetch(url, {
@@ -28,7 +32,9 @@ const Payment = () => {
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
-
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
                 </div>
             </div>
         </div>
